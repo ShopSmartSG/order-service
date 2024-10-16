0948552b493201aa922bbf4cb192abb90840d8fb
Anvisimi <8444.sa@gmail.com>
2024-10-16 14:41:35 +0800

    added route

---
+++ src/main/java/sg/edu/nus/iss/order_service/OrderServiceApplication.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.iss.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
